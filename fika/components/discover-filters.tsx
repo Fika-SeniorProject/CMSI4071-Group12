701b2b17f4d077763ab05d2d7fc7800d8112fa34
k-galvin <110426866+k-galvin@users.noreply.github.com>
2025-10-04 17:39:24 -0700

    Update discover-filters.tsx

--- fika/components/discover-filters.tsx
+++ fika/components/discover-filters.tsx
@@ -1,15 +1,22 @@
-'use client';
+"use client";
 
 import { FilterDropdown } from "@/components/ui/filter-dropdown";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
 
-export function DiscoverFilters({ cities, parkings, seatings, vibes }: { cities: string[], parkings: string[], seatings: string[], vibes: string[] }) {
+export function DiscoverFilters({
+  cities,
+  parkings,
+  seatings,
+  vibes,
+}: {
+  cities: string[];
+  parkings: string[];
+  seatings: string[];
+  vibes: string[];
+}) {
   const router = useRouter();
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-
 
   const clearFilters = () => {
     router.push(pathname);
@@ -21,9 +28,19 @@ export function DiscoverFilters({ cities, parkings, seatings, vibes }: { cities:
       <FilterDropdown title="Parking" options={parkings} filterKey="parking" />
       <FilterDropdown title="Seating" options={seatings} filterKey="seating" />
       <FilterDropdown title="Vibe" options={vibes} filterKey="vibe" />
-      <FilterDropdown title="Wifi" options={["Yes", "No"]} filterKey="has_wifi" />
-      <FilterDropdown title="Outlets" options={["Yes", "No"]} filterKey="has_outlets" />
-      <Button variant="ghost" onClick={clearFilters}>Clear Filters</Button>
+      <FilterDropdown
+        title="Wifi"
+        options={["Yes", "No"]}
+        filterKey="has_wifi"
+      />
+      <FilterDropdown
+        title="Outlets"
+        options={["Yes", "No"]}
+        filterKey="has_outlets"
+      />
+      <Button variant="ghost" onClick={clearFilters}>
+        Clear Filters
+      </Button>
     </div>
   );
 }
